e80809202f1f7e9f5b5959fd11a64f79fe43c98a
Ishan Chinthaka <ishanchinthaka2002@gmail.com>
2026-02-27 16:55:39 +0530

    change the cv

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    // Typing animation
+    new Typed('.typing', {
+        strings: ["Developer", "Software Engineer", "UI/UX Enthusiast"],
+        typeSpeed: 60,
+        backSpeed: 40,
+        loop: true
+    });
+
+    // Smooth scroll
+    $('.nav-menu a').on('click', function(e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+
+});
